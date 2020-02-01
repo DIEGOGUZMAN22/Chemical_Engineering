7207ed8bc65de545f21ebc2ceaf5eeb387d28247
DIEGOGUZMAN22 <60535007+DIEGOGUZMAN22@users.noreply.github.com>
2020-02-01 12:52:04 -0600

    Update Runge Kutta 4th.jl

--- Runge Kutta 4th.jl
+++ Runge Kutta 4th.jl
@@ -1,16 +1,16 @@
 function RK4(ODEfun,tspan,y0,h)
-    n = (xf - x0)/h + 1
+    n = (tspan[end] - tspan[1])/h + 1
     y = zeros(Int(n), length(y0))
     for j = 1:length(y0)
         y[1,j] = y0[j]
     end
-    x = collect(x0:h:xf)
+    x = collect(tspan[1]:h:tspan[end])
     for j = 1:length(x)-1
-        k1 = zeros(neq)
-        k2 = zeros(neq)
-        k3 = zeros(neq)
-        k4 = zeros(neq)
-        for i = 1:neq
+        k1 = zeros(length(y0))
+        k2 = zeros(length(y0))
+        k3 = zeros(length(y0))
+        k4 = zeros(length(y0))
+        for i = 1:length(y0)
             k1[i] = f(x[j],y[j,:])[i]
             k2[i] = f(x[j] + (1/2) * h, y[j,:] + (1/2) * k1[:] * h)[i]
             k3[i] = f(x[j] + (1/2) * h, y[j,:] + (1/2) * k2[:] * h)[i]
